2d8f621d80413b0ae137dab1feec19d524ef4b7e
Chinmaykd21 <chinmaykd21@gmail.com>
2021-04-01 00:12:22 +0000

    Backup for release 1

---
+++ rust_compiler/src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+// *************************************************************************************************************************************
+// Author: Chinmay Kulkarni                                                                                                            *
+// Project: Designing a compiler with Rust                                                                                             *
+// Code Snippet Description: This section of code will accept input from the command line (which will be the path to the input file    *
+//                           which we want to check with our compiler). The code will try to open the file, if the file is opened then *
+//                           read the contents of the file. If for some reason the file cannot be read, then close the program with    * 
+//                           appropriate message. If the number of arguments is more than expected (i.e., more than path to the file)  *
+//                           then exit the program with appropriate message.                                                           *
+// Reference: https://www.youtube.com/watch?v=nQqraiMymcU                                                                              *
+// *************************************************************************************************************************************
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+ 
+    if args.len() > 1 {
+        if args.len() == 2 {
+            let mut file = match File::open(&args[1]) {
+                Ok(file) => file,
+                Err(why) => panic!("\n\n{}:{}\n\n", why, &args[1]),
+            };
+ 
+            let mut contents = String::new();
+            
+            match file.read_to_string(&mut contents) {
+                Ok(_) => println!("File Contents\n\n{}", contents),
+                Err(why) => panic!("\n\nCould not read the contents of the file at path {} due to {}\n\n", &args[1], why),
+            };
+ 
+        } else {
+            println!("More than required arguments provided:{:?}",args);
+        }
+ 
+    } else {
+        println!("Please provide a file name");
+    }
+}
---
+++ rust_compiler/target/debug/deps/rust_compiler-80feef013638e6b8.d
@@ -0,0 +1,5 @@
+/home/student/Rust-Compiler/rust_compiler/target/debug/deps/rust_compiler-80feef013638e6b8.rmeta: src/main.rs
+
+/home/student/Rust-Compiler/rust_compiler/target/debug/deps/rust_compiler-80feef013638e6b8.d: src/main.rs
+
+src/main.rs:
